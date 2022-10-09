f61fe181a3e75cf1b0937935bf4e34305d7c594d
Rafael Mih <rafaelwsantos@gmail.com>
2022-10-08 23:33:19 -0300

    title

--- presentation/src/App.tsx
+++ presentation/src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
 import Avatar from "./components/Avatar/Avatar";
 import Presentation from "./components/Presentation/Presentation";
