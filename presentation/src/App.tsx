48776df5964807a523034adf4b7a5fb0656db289
tchfer <tchfernando@gmail.com>
2022-10-10 02:29:57 -0300

    Add person description to app

--- presentation/src/App.tsx
+++ presentation/src/App.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 import "./App.css";
 import Avatar from "./components/Avatar/Avatar";
+import Cep from "./components/Cep/Cep";
 import Presentation from "./components/Presentation/Presentation";
 import { IPerson, persons } from "./interfaces/IPerson";
 
@@ -31,6 +32,30 @@ const App = () => {
           <div className="col-md-5 presentation">
             {person ? <Presentation person={person} /> : null}
           </div>
+        </div>
+      </div>
+      <div className="description-wrapper">
+        <div className="about-wrapper">
+          <h3>Sobre {person?.name.split(" ")[0]}</h3>
+          <div className="description">{person?.about}</div>
+        </div>
+        <div className="about-wrapper">
+          <h3>Endereço</h3>
+          <div className="description">
+            <Cep cep={person?.cep} />
+          </div>
+        </div>
+        <div className="about-wrapper">
+          <h3>Formação</h3>
+          <div className="description">{person?.graduation}</div>
+        </div>
+        <div className="about-wrapper">
+          <h3>Experiência</h3>
+          <div className="description">{person?.experience}</div>
+        </div>
+        <div className="about-wrapper">
+          <h3>Hobbies</h3>
+          <div className="description">{person?.hobbie}</div>
         </div>
       </div>
     </div>
